ea97b85c442b3f52d7f36e499ca30a5003682b34
David G <david.a.grs@gmail.com>
2017-09-30 20:21:35 +0200

    model: cleanup, using delegate

--- table_model.cc
+++ table_model.cc
@@ -1,8 +1,15 @@
 #include "table_model.h"
 
+#include <iostream>
+
+TableModel::TableModel()
+{
+	mFilters.resize(columnCount());
+}
+
 int TableModel::rowCount(const QModelIndex&) const
 {
-	return 2;
+	return 3;
 }
 
 int TableModel::columnCount(const QModelIndex&) const
@@ -12,19 +19,45 @@ int TableModel::columnCount(const QModelIndex&) const
 
 QVariant TableModel::data(const QModelIndex& index, int role) const
 {
-	switch (role)
+	if (role == Qt::EditRole && index.row() == 0)
 	{
-		case Qt::DisplayRole:
+		return mFilters[index.column()];
+	}
+
+	if (role == Qt::DisplayRole)
+	{
+		if (index.row() == 0)
+		{
+			return mFilters[index.column()];
+		}
+		else
 		{
 			return 4 - index.row() + index.column();
 		}
 	}
-	return QVariant();
+
+	return {};
+}
+
+Qt::ItemFlags TableModel::flags(const QModelIndex& index) const
+{
+	return Qt::ItemIsEnabled | (index.row() == 0 ? Qt::ItemIsEditable : Qt::ItemIsSelectable);
 }
 
 QModelIndex TableModel::index(int row, int column, const QModelIndex&) const
 {
 	return QAbstractItemModel::createIndex(row, column, Q_NULLPTR);
+}
+
+bool TableModel::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+	if (index.row() == 0 && (role == Qt::UserRole || role == Qt::EditRole))
+	{
+		mFilters[index.column()] = value.toString();
+		std::cout <<mFilters[index.column()].toStdString() << std::endl;
+	}
+
+	return true;
 }
 
 void TableModel::sort(int column, Qt::SortOrder order)
--- table_model.h
+++ table_model.h
@@ -2,15 +2,25 @@
 
 #include <QAbstractTableModel>
 
+#include <vector>
+
 class TableModel : public QAbstractItemModel
 {
 public:
-	int rowCount(const QModelIndex& parent) const override;
-	int columnCount(const QModelIndex& parent) const override;
+	TableModel();
+
+	int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+	int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
 	QVariant data(const QModelIndex& index, int role) const override;
+	Qt::ItemFlags flags(const QModelIndex& index) const override;
 
 	void sort(int column, Qt::SortOrder order) override;
+	bool setData(const QModelIndex& index, const QVariant& value, int role);
 
 	QModelIndex index(int row, int column, const QModelIndex& parent) const override;
 	QModelIndex parent(const QModelIndex&) const override { return {}; }
+
+private:
+	std::vector<QString> mFilters;
 };
