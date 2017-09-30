a85c73b5dc7aa39c5103ca2dd94d5b82023633f2
David G <david.a.grs@gmail.com>
2017-09-30 17:10:07 +0200

    initial commit

---
+++ main.cc
@@ -0,0 +1,25 @@
+#include "table_model.h"
+
+#include <QApplication>
+#include <QTableView>
+#include <QTableView>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+
+	QTableView table;
+	TableModel model;
+
+	QSortFilterProxyModel proxyModel;
+	proxyModel.setSourceModel(&model);
+	table.setModel(&proxyModel);
+
+	table.setModel(&model);
+	table.setSortingEnabled(true);
+	table.sortByColumn(0, Qt::AscendingOrder);
+	table.reset();
+	table.show();
+
+	return a.exec();
+}
