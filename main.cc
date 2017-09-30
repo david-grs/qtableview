eaa6ae2cdfbd0a26d8bba1745e02855a00d48423
David G <david.a.grs@gmail.com>
2017-09-30 20:21:23 +0200

    adding selection mode, triggers, delegate

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include "table_model.h"
+#include "table_view.h"
 
 #include <QApplication>
 #include <QTableView>
@@ -9,14 +10,19 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
-	QTableView table;
+	TableView table;
 	TableModel model;
+	TableDelegate delegate(model);
 
 	//QSortFilterProxyModel proxyModel;
 	//proxyModel.setSourceModel(&model);
 	table.setModel(&model);
+	table.setItemDelegateForRow(0, &delegate);
 
 	table.setSortingEnabled(true);
+	table.setSelectionBehavior(QAbstractItemView::SelectRows);
+	table.setSelectionMode(QAbstractItemView::ExtendedSelection);
+	table.setEditTriggers(QAbstractItemView::AllEditTriggers);
 	table.sortByColumn(0, Qt::AscendingOrder);
 	table.reset();
 	table.show();
